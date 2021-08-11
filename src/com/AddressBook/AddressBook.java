8f0b558058ae09ea61ca1b97a4ea6b3b784ee9f6
SRC0747 <srcsrk1998@gmail.com>
2021-08-11 22:23:12 +0530

    added: Taking the details from the user and storing these details.

--- src/com/AddressBook/AddressBook.java
+++ src/com/AddressBook/AddressBook.java
@@ -4,29 +4,29 @@ public class AddressBook {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Address Book System System.");
-        detailBook();
+        personDetail();
     }
-    public static void detailBook()
+    public static void personDetail()
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the name of the book:");
+        System.out.println("Enter the name of the person:");
         System.out.println("Enter the name of the address:");
         System.out.println("Enter the name of the City:");
         System.out.println("Enter the name of the State:");
         System.out.println("Enter the name of the Contact Number:");
         System.out.println("Enter the name of the email id:");
         String name = sc.nextLine();
-        System.out.println("Name of the book is:"+name);
+        System.out.println("Name of the person is:"+name);
         String address = sc.nextLine();
-        System.out.println("Name of the book is:"+address);
+        System.out.println("Name of the person is:"+address);
         String city = sc.nextLine();
-        System.out.println("Name of the book is:"+city);
+        System.out.println("Name of the person is:"+city);
         String state = sc.nextLine();
-        System.out.println("Name of the book is:"+state);
+        System.out.println("Name of the person is:"+state);
         int ph_no=sc.nextInt();
-        System.out.println("Name of the book is:"+ph_no);
+        System.out.println("Name of the person is:"+ph_no);
         String email_id = sc.nextLine();
-        System.out.println("Name of the book is:"+email_id);
+        System.out.println("Name of the person is:"+email_id);
 
     }
 }
